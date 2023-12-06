6f0c4cb24965d376167b291f515482e89e26e20c
Johnny Vaz <johnnyjohnnyjohnnyvaz@gmail.com>
2023-12-06 16:32:23 -0300

    feat: pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import partytown from '@astrojs/partytown'
+import partytown from '@astrojs/partytown';
+import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://johnnyvaz.com.br',
-  integrations: [mdx(), sitemap(), tailwind(),
+  integrations: [mdx(), sitemap(), tailwind(), AstroPWA(),
     partytown({
 			config: {
 			  forward: ["dataLayer.push"],
